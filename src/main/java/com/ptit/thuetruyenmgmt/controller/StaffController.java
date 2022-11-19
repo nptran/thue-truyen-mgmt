2e52bd8236b1efaea0b04ab608751b464590ba02
nphuctran <phuc.tranngoc@nttdata.com>
2022-11-20 00:24:29 +0700

    Làm mịn sơ StaffController

--- src/main/java/com/ptit/thuetruyenmgmt/controller/StaffController.java
+++ src/main/java/com/ptit/thuetruyenmgmt/controller/StaffController.java
@@ -3,11 +3,16 @@ package com.ptit.thuetruyenmgmt.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class StaffController {
 
-    @RequestMapping(path= {"/", "home"})
-    public String home() {
+    @RequestMapping(path = {"/", "home"})
+    public String home(HttpSession session) {
+        // Lưu lại các thông tin này sau khi NV đăng nhập thành công
+        session.setAttribute("loginCode", "NV00651");
+        session.setAttribute("id", 1);
         return "gd-chinh-nv";
     }
 
