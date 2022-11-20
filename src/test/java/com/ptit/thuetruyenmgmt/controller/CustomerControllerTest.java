f4153161b368deab60ab3f5c20ddf7882c7092cb
nphuctran <phuc.tranngoc@nttdata.com>
2022-11-20 16:21:13 +0700

    Làm mịn CustomerControllerTest

--- src/test/java/com/ptit/thuetruyenmgmt/controller/CustomerControllerTest.java
+++ src/test/java/com/ptit/thuetruyenmgmt/controller/CustomerControllerTest.java
@@ -4,27 +4,20 @@ import com.ptit.thuetruyenmgmt.model.Customer;
 import com.ptit.thuetruyenmgmt.model.FullName;
 import com.ptit.thuetruyenmgmt.service.CustomerService;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
-//import org.mockito.BDDMockito;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.is;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,16 +32,28 @@ public class CustomerControllerTest {
     private MockMvc mvc;
 
 
+    /**
+     * {@link CustomerController#getGDSearch()}: Test Case 1
+     *
+     * @POSITIVE: NV đã nhấn chọn Tìm kiếm KH trên Trang chủ
+     * → Hiển thị giao diện tìm KH
+     */
     @Test
     public void whenGetGDSearch_thenReturnStatus200AndSearchView() throws Exception {
         this.mvc.perform(get("/customer"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("search"));
+                .andExpect(view().name("gd-tim-kh"));
     }
 
 
+    /**
+     * {@link CustomerController#searchCustomer(String, HttpSession)}: Test Case 1
+     *
+     * @POSITIVE: NV đã nhập tên KH (`Minh`) và nhấn Tìm kiếm
+     * → DS KH với tên Minh
+     */
     @Test
-    public void whenSearchCustomer_thenReturnStatus200AndCustomers() throws Exception {
+    public void whenSearchCustomer_thenReturnStatus200AndMatchedCustomers() throws Exception {
         String kw = "Minh";
         // Giả định service trả về 2 KH tên Minh
         Customer c1 = Customer.builder().id(1).fullName(new FullName(1, "Minh", "Phạm")).build();
@@ -60,7 +65,7 @@ public class CustomerControllerTest {
         when(service.getCustomerByName(kw)).thenReturn(customers);
         this.mvc.perform(get("/customer/search?kw-name=" + kw))
                 .andExpect(status().isOk())
-                .andExpect(view().name("search"))
+                .andExpect(view().name("gd-tim-kh"))
                 .andExpect(model().attribute("customers", hasSize(2)))
                 .andExpect(model().attribute("customers", hasItem(
                         allOf(
@@ -78,4 +83,44 @@ public class CustomerControllerTest {
         verify(service, times(1)).getCustomerByName(kw);
         verifyNoMoreInteractions(service);
     }
+
+
+    /**
+     * {@link CustomerController#searchCustomer(String, HttpSession)}: Test Case 2
+     *
+     * @POSITIVE: NV không nhập tên và nhấn Tìm kiếm
+     * → DS tất cả KH
+     */
+    @Test
+    public void whenSearchCustomer_thenReturnStatus200AndAllCustomers() throws Exception {
+        String kw = "";
+        // Giả định service trả về 2 KH tên Minh
+        Customer c1 = Customer.builder().id(1).fullName(new FullName(1, "Đạt", "Phạm")).build();
+        Customer c2 = Customer.builder().id(2).fullName(new FullName(2, "Nam", "Trần")).build();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(c1);
+        customers.add(c2);
+
+        when(service.getCustomerByName(kw)).thenReturn(customers);
+        this.mvc.perform(get("/customer/search?kw-name=" + kw))
+                .andExpect(status().isOk()) // Mong đợi vẫn hoạt động được
+                .andExpect(view().name("gd-tim-kh"))
+                .andExpect(model().attribute("customers", hasSize(2)))
+                .andExpect(model().attribute("customers", hasItem(
+                        allOf(
+                                hasProperty("id", Matchers.is(1)),
+                                hasProperty("fullName", Matchers.hasToString(c1.getFullName().toString()))
+                        )
+                )))
+                .andExpect(model().attribute("customers", hasItem(
+                        allOf(
+                                hasProperty("id", Matchers.is(2)),
+                                hasProperty("fullName", Matchers.hasToString(c2.getFullName().toString()))
+                        )
+                )));
+
+        verify(service, times(1)).getCustomerByName(kw);
+        verifyNoMoreInteractions(service);
+    }
+
 }
