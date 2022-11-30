c9fc93eae9c05310988f20ddc31889722de9c715
nphuctran <phuc.tranngoc@nttdata.com>
2022-11-30 11:43:03 +0700

    Cập nhật Unit test cho Bill entity 100%

--- src/test/java/com/ptit/thuetruyenmgmt/entity/BillTest.java
+++ src/test/java/com/ptit/thuetruyenmgmt/entity/BillTest.java
@@ -1,11 +1,10 @@
 package com.ptit.thuetruyenmgmt.entity;
 
-import com.ptit.thuetruyenmgmt.model.Bill;
+import com.ptit.thuetruyenmgmt.model.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.ptit.thuetruyenmgmt.model.RentedBook;
-import com.ptit.thuetruyenmgmt.model.Staff;
-import io.swagger.models.auth.In;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,18 +16,106 @@ import java.util.List;
 @SpringBootTest
 class BillTest {
 
-//    private Bill bill;
+    private Bill bill;
+
+    private Integer expectedId;
+
+    private double expectedTotalAmount;
+
+    private LocalDateTime expectedCreateTime;
+
+    private List<RentedBook> expectedRentedBooks;
+
+    private Staff expectedStaff;
+
+    private Field id;
+    private Field totalAmount;
+    private Field createTime;
+    private Field rentedBooks;
+    private Field staff;
+
+    /**
+     * GIVEN
+     */
+    @BeforeEach
+    void initData() throws NoSuchFieldException {
+        bill = new Bill();
+        expectedId = 1;
+        expectedTotalAmount = 100.99;
+        expectedCreateTime = LocalDateTime.of(2022, 10, 10, 10, 0, 0);
+
+        expectedRentedBooks = new ArrayList<>();
+        RentedBook book1 = RentedBook.builder()
+                .id(1)
+                .rentedTime(LocalDateTime.of(2022, 10, 8, 8, 0, 0))
+                .bookTitle(BookTitle.builder().id(1000).build())
+                .customer(Customer.builder().id(200).build())
+                .bill(bill)
+                .isPaid(true)
+                .build();
+        RentedBook book2 = RentedBook.builder()
+                .id(2)
+                .rentedTime(LocalDateTime.of(2022, 10, 8, 8, 0, 0))
+                .bookTitle(BookTitle.builder().id(2000).build())
+                .customer(Customer.builder().id(200).build())
+                .bill(bill)
+                .isPaid(true)
+                .build();
+        expectedRentedBooks.add(book1);
+        expectedRentedBooks.add(book2);
+
+        expectedStaff = Staff.staffBuilder().id(10).fullName(new FullName(1, "An", "Phạm")).build();
+
+        id = bill.getClass().getDeclaredField("id");
+        totalAmount = bill.getClass().getDeclaredField("totalAmount");
+        createTime = bill.getClass().getDeclaredField("createTime");
+        rentedBooks = bill.getClass().getDeclaredField("rentedBooks");
+        staff = bill.getClass().getDeclaredField("staff");
+        // Allow accessible on private fields
+        id.setAccessible(true);
+        totalAmount.setAccessible(true);
+        createTime.setAccessible(true);
+        rentedBooks.setAccessible(true);
+        staff.setAccessible(true);
+    }
+
 
     @Test
-    void testSetters() throws NoSuchFieldException, IllegalAccessException {
-        // GIVEN
-        final Bill bill = new Bill();
-        Integer expectedId = 1;
-        double expectedTotalAmount = 100;
-        LocalDateTime expectedCreateTime = LocalDateTime.now();
-        List<RentedBook> expectedRentedBooks = new ArrayList<>();
-        Staff expectedStaff = new Staff();
+    void testFullArgConstructor() throws IllegalAccessException {
+        // WHEN
+        bill = new Bill(expectedId, expectedTotalAmount, expectedCreateTime, expectedRentedBooks, expectedStaff);
 
+        // THEN
+        assertEquals(expectedId, id.get(bill));
+        assertEquals(expectedTotalAmount, totalAmount.get(bill));
+        assertEquals(expectedCreateTime, createTime.get(bill));
+        assertEquals(expectedRentedBooks, rentedBooks.get(bill));
+        assertEquals(expectedStaff, staff.get(bill));
+    }
+
+
+    @Test
+    void testBuilder() throws IllegalAccessException {
+        // WHEN
+        bill = Bill.builder()
+                .id(expectedId)
+                .totalAmount(expectedTotalAmount)
+                .createTime(expectedCreateTime)
+                .rentedBooks(expectedRentedBooks)
+                .staff(expectedStaff)
+                .build();
+
+        // THEN
+        assertEquals(expectedId, id.get(bill));
+        assertEquals(expectedTotalAmount, totalAmount.get(bill));
+        assertEquals(expectedCreateTime, createTime.get(bill));
+        assertEquals(expectedRentedBooks, rentedBooks.get(bill));
+        assertEquals(expectedStaff, staff.get(bill));
+    }
+
+
+    @Test
+    void testSetters() throws IllegalAccessException {
         // WHEN
         bill.setId(expectedId);
         bill.setTotalAmount(expectedTotalAmount);
@@ -37,17 +124,6 @@ class BillTest {
         bill.setStaff(expectedStaff);
 
         // THEN
-        final Field id = bill.getClass().getDeclaredField("id");
-        final Field totalAmount = bill.getClass().getDeclaredField("totalAmount");
-        final Field createTime = bill.getClass().getDeclaredField("createTime");
-        final Field rentedBooks = bill.getClass().getDeclaredField("rentedBooks");
-        final Field staff = bill.getClass().getDeclaredField("staff");
-        // Allow accessible on private fields
-        id.setAccessible(true);
-        totalAmount.setAccessible(true);
-        createTime.setAccessible(true);
-        rentedBooks.setAccessible(true);
-        staff.setAccessible(true);
         assertEquals(expectedId, id.get(bill));
         assertEquals(expectedTotalAmount, totalAmount.get(bill));
         assertEquals(expectedCreateTime, createTime.get(bill));
@@ -55,49 +131,23 @@ class BillTest {
         assertEquals(expectedStaff, staff.get(bill));
     }
 
+
     @Test
-    void testGetters() throws NoSuchFieldException, IllegalAccessException {
-        // Given
-        final Bill bill = new Bill();
-        final Field id = bill.getClass().getDeclaredField("id");
-        final Field totalAmount = bill.getClass().getDeclaredField("totalAmount");
-        final Field createTime = bill.getClass().getDeclaredField("createTime");
-        final Field rentedBooks = bill.getClass().getDeclaredField("rentedBooks");
-        final Field staff = bill.getClass().getDeclaredField("staff");
-        // Allow accessible on private fields
-        id.setAccessible(true);
-        totalAmount.setAccessible(true);
-        createTime.setAccessible(true);
-        rentedBooks.setAccessible(true);
-        staff.setAccessible(true);
-
-
-        Integer expectedId = 1;
-        double expectedTotalAmount = 100;
-        LocalDateTime expectedCreateTime = LocalDateTime.now();
-        List<RentedBook> expectedRentedBooks = new ArrayList<>();
-        Staff expectedStaff = new Staff();
-
+    void testGetters() throws IllegalAccessException {
+        // WHEN
         id.set(bill, expectedId);
         totalAmount.set(bill, expectedTotalAmount);
         createTime.set(bill, expectedCreateTime);
         rentedBooks.set(bill, expectedRentedBooks);
         staff.set(bill, expectedStaff);
 
-        // When
-        //TODO: https://stackoverflow.com/questions/21354311/junit-test-of-setters-and-getters-of-instance-variables
-        Integer actualId = id.getValue();
-        double actualTotalAmount = 100;
-        LocalDateTime actualCreateTime = LocalDateTime.now();
-        List<RentedBook> actualRentedBooks = new ArrayList<>();
-        Staff actualStaff = new Staff();
-
-        // Then
-        assertEquals(expectedId, id.get(bill));
-        assertEquals(expectedTotalAmount, totalAmount.get(bill));
-        assertEquals(expectedCreateTime, createTime.get(bill));
-        assertEquals(expectedRentedBooks, rentedBooks.get(bill));
-        assertEquals(expectedStaff, staff.get(bill));
+        // THEN
+        assertEquals(expectedId, bill.getId());
+        assertEquals(expectedTotalAmount, bill.getTotalAmount());
+        assertEquals(expectedCreateTime, bill.getCreateTime());
+        assertEquals(expectedRentedBooks, bill.getRentedBooks());
+        assertEquals(expectedStaff, bill.getStaff());
     }
+
 
 }
