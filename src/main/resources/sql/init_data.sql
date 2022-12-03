fb40f00fbc68d640b010b7e0abc38fc1413a42b3
nphuctran <phuc.tranngoc@nttdata.com>
2022-12-03 13:18:02 +0700

    Test-flow: Cập nhật init-data.sql

--- src/main/resources/sql/init_data.sql
+++ src/main/resources/sql/init_data.sql
@@ -67,22 +67,22 @@ VALUES
 -- BOOK TITLE
 INSERT INTO `thue-truyen-mgmt`.`book_title`
 (`id`,`author`, `code`, `price`, `quantity`, `title_name`, `year`, `publisher_id`)
-VALUES (11, 'Phạm Ánh Nhật', '1232435467EE', '50000', '99', 'Hoa Vàng', '2019', '1'),
-       (12, 'JK. Rowling', '1223756434AA', '120000', '99', 'Harry Potter Tập 1', '2006', 1),
-       (13, 'JK. Rowling', 'A34656UUH656', '120000', '99', 'Harry Potter Tập 2', '2002', 1),
-       (14, 'Nhiều Tác Giả', 'H4YT84022222', '200000', '99', 'Bách Khoa Toàn Thư', '2010', 1),
-       (21, 'Fujiko F.Fujio', 'HF383053FJ99', '20000', '99', 'Doraemon 1', '2010', 2),
-       (22, 'Fujiko F.Fujio', 'FYH4834932UR', '20000', '99', 'Doraemon 2', '2010', 2),
-       (23, 'Fujiko F.Fujio', '385493URF902', '20000', '99', 'Doraemon 3', '2010', 2),
-       (24, 'Fujiko F.Fujio', 'HGV832U93EFJ', '20000', '99', 'Doraemon 4', '2010', 2),
-       (31, 'Phạm Vũ', 'U5843EF90D32', '15000', '99', 'Quán Cóc', '2010', 3),
-       (32, 'Trần Thế Ký', 'JF43T90923F3', '30000', '99', 'Ước mơ nhỏ', '2010', 3),
-       (33, 'Tô Hoài', 'FF923U9TJVE3', '50000', '99', 'Dế Choắt Phiêu Lưu Ký', '2010', 3),
-       (34, 'Nam Cao', '325UF89219ER', '120000', '99', 'Tuyển tập Nam Cao', '2010', 3),
-       (41, 'Test', 'U359UF90329R', '10000', '99', 'Truyện 1', '2022', 4),
-       (42, 'Test', '3I950RF9292F', '10000', '99', 'Truyện 2', '2022', 4),
-       (43, 'Test', '2935I9F90DFW', '10000', '99', 'Truyện 3', '2022', 4),
-       (44, 'Test', '32950UF90000', '10000', '99', 'Truyện 4', '2022', 4);
+VALUES (11, 'Phạm Ánh Nhật', '1232435467EE', '50000', 99, 'Hoa Vàng', '2019', '1'),
+       (12, 'JK. Rowling', '1223756434AA', '120000', 99, 'Harry Potter Tập 1', '2006', 1),
+       (13, 'JK. Rowling', 'A34656UUH656', '120000', 99, 'Harry Potter Tập 2', '2002', 1),
+       (14, 'Nhiều Tác Giả', 'H4YT84022222', '200000', 99, 'Bách Khoa Toàn Thư', '2010', 1),
+       (21, 'Fujiko F.Fujio', 'HF383053FJ99', '20000', 99, 'Doraemon 1', '2010', 2),
+       (22, 'Fujiko F.Fujio', 'FYH4834932UR', '20000', 99, 'Doraemon 2', '2010', 2),
+       (23, 'Fujiko F.Fujio', '385493URF902', '20000', 99, 'Doraemon 3', '2010', 2),
+       (24, 'Fujiko F.Fujio', 'HGV832U93EFJ', '20000', 99, 'Doraemon 4', '2010', 2),
+       (31, 'Phạm Vũ', 'U5843EF90D32', '15000', 99, 'Quán Cóc', '2010', 3),
+       (32, 'Trần Thế Ký', 'JF43T90923F3', '30000', 99, 'Ước mơ nhỏ', '2010', 3),
+       (33, 'Tô Hoài', 'FF923U9TJVE3', '50000', 99, 'Dế Choắt Phiêu Lưu Ký', '2010', 3),
+       (34, 'Nam Cao', '325UF89219ER', '120000', 99, 'Tuyển tập Nam Cao', '2010', 3),
+       (41, 'Test', 'U359UF90329R', '10000', 99, 'Truyện 1', '2022', 4),
+       (42, 'Test', '3I950RF9292F', '10000', 99, 'Truyện 2', '2022', 4),
+       (43, 'Test', '2935I9F90DFW', '10000', 99, 'Truyện 3', '2022', 4),
+       (44, 'Test', '32950UF90000', '10000', 99, 'Truyện 4', '2022', 4);
 
 -- STAFF
 INSERT INTO `thue-truyen-mgmt`.`user`
